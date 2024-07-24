cd69635b6b853c6a46299a3e57c6680a727fc727
David Gomez <info@dagdev.com.ar>
2024-07-24 19:41:32 -0300

    Verificación de existencia previa claves en array

--- functions.php
+++ functions.php
@@ -2,7 +2,8 @@
 
 // style and scripts
 add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
-function bootscore_child_enqueue_styles() {
+function bootscore_child_enqueue_styles()
+{
 
   // style.css
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
@@ -16,37 +17,49 @@ function bootscore_child_enqueue_styles() {
 }
 
 // Restaura la funcionalidad de widgets clásicos y desactiva el widget-block-editor
-function restore_classic_widgets() {
-  remove_theme_support( 'widgets-block-editor' ); // Restaura widgets clásicos
+function restore_classic_widgets()
+{
+  remove_theme_support('widgets-block-editor'); // Restaura widgets clásicos
 }
 
-add_action( 'after_setup_theme', 'restore_classic_widgets' );
+add_action('after_setup_theme', 'restore_classic_widgets');
 
 
 // Filtrar las clases del elemento li
-function top_footer_menu_li_classes($classes, $item, $args, $depth) {
+function top_footer_menu_li_classes($classes, $item, $args, $depth)
+{
   $classes[] = 'nav-item';
 
   return $classes;
 }
 
 // Filtra clases de los <a> internos
-function top_footer_menu_link_attributes($atts, $item, $args, $depth) {
-  $atts['class'] .= ' nav-link link-light';
-  
+function top_footer_menu_link_attributes($atts, $item, $args, $depth)
+{
+  if (array_key_exists('class', $atts)) {
+    $atts['class'] .= ' nav-link link-light';
+  } else {
+    $atts['class'] = 'nav-link link-light';
+  }
+
   return $atts;
 }
 
 // Filtrar las opciones del menú dentro de un widget
-function top_footer_menu_args($nav_menu_args, $nav_menu, $args, $instance) {
+function top_footer_menu_args($nav_menu_args, $nav_menu, $args, $instance)
+{
   // Verificar si el widget es el que deseas
   if ($args['widget_id'] === 'nav_menu-2') {
-      // Agregar clases al elemento ul
+    // Agregar clases al elemento ul
+    if (array_key_exists('menu_class', $nav_menu_args)) {
       $nav_menu_args['menu_class'] .= ' nav';
+    } else {
+      $nav_menu_args['menu_class'] = ' nav';
+    }
 
-      // Agregar clases al elemento li
-      add_filter('nav_menu_css_class', 'top_footer_menu_li_classes', 10, 4);
-      add_filter('nav_menu_link_attributes', 'top_footer_menu_link_attributes', 10, 4);
+    // Agregar clases al elemento li
+    add_filter('nav_menu_css_class', 'top_footer_menu_li_classes', 10, 4);
+    add_filter('nav_menu_link_attributes', 'top_footer_menu_link_attributes', 10, 4);
   }
 
   return $nav_menu_args;
@@ -56,12 +69,13 @@ add_filter('widget_nav_menu_args', 'top_footer_menu_args', 10, 4);
 
 
 
-function top_footer_widget_classes($params) {
+function top_footer_widget_classes($params)
+{
   // Verificar si es la zona para widgets específica
   if ($params[0]['id'] === 'top-footer') {
-      // Realizar las modificaciones necesarias en las clases de la zona
-      // Por ejemplo, puedes modificar las clases de la zona aquí
-      $params[0]['before_widget'] = preg_replace('/class="(.*?)mb-5(.*?)"/', 'class="$1$2"', $params[0]['before_widget']);
+    // Realizar las modificaciones necesarias en las clases de la zona
+    // Por ejemplo, puedes modificar las clases de la zona aquí
+    $params[0]['before_widget'] = preg_replace('/class="(.*?)mb-5(.*?)"/', 'class="$1$2"', $params[0]['before_widget']);
   }
 
   return $params;
@@ -70,14 +84,15 @@ function top_footer_widget_classes($params) {
 add_filter('dynamic_sidebar_params', 'top_footer_widget_classes');
 
 // Agregar page-slug a la función body_class()
-function page_slug_body_class( $classes ) {
+function page_slug_body_class($classes)
+{
   global $post;
-  
-  if ( isset( $post ) ) {
+
+  if (isset($post)) {
     $classes[] = $post->post_type . '-' . $post->post_name;
   }
-  
+
   return $classes;
 }
 
-add_filter( 'body_class', 'page_slug_body_class' );
+add_filter('body_class', 'page_slug_body_class');
